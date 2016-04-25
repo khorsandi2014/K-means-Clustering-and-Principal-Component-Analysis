8061dc268ff574993524cb4a8aa06874a42b47dc
khorsandi2014 <rahman.khorsandi@gmail.com>
2016-04-25 16:50:49 -0500

    Update ex7.m

--- ex7.m
+++ ex7.m
@@ -1,31 +1,8 @@
-%% Machine Learning Online Class
-%  Exercise 7 | Principle Component Analysis and K-Means Clustering
-%
-%  Instructions
-%  ------------
-%
-%  This file contains code that helps you get started on the
-%  exercise. You will need to complete the following functions:
-%
-%     pca.m
-%     projectData.m
-%     recoverData.m
-%     computeCentroids.m
-%     findClosestCentroids.m
-%     kMeansInitCentroids.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
 
 %% Initialization
 clear ; close all; clc
 
-%% ================= Part 1: Find Closest Centroids ====================
-%  To help you implement K-Means, we have divided the learning algorithm 
-%  into two functions -- findClosestCentroids and computeCentroids. In this
-%  part, you shoudl complete the code in the findClosestCentroids function. 
-%
+
 fprintf('Finding closest centroids.\n\n');
 
 % Load an example dataset that we will be using
@@ -46,10 +23,6 @@ fprintf('\n(the closest centroids should be 1, 3, 2 respectively)\n');
 fprintf('Program paused. Press enter to continue.\n');
 % pausre;
 
-%% ===================== Part 2: Compute Means =========================
-%  After implementing the closest centroids function, you should now
-%  complete the computeCentroids function.
-%
 fprintf('\nComputing centroids means.\n\n');
 
 %  Compute means based on the closest centroids found in the previous part.
@@ -65,13 +38,6 @@ fprintf('   [ 7.119387 3.616684 ]\n\n');
 fprintf('Program paused. Press enter to continue.\n');
 % pause;
 
-
-%% =================== Part 3: K-Means Clustering ======================
-%  After you have completed the two functions computeCentroids and
-%  findClosestCentroids, you have all the necessary pieces to run the
-%  kMeans algorithm. In this part, you will run the K-Means algorithm on
-%  the example dataset we have provided. 
-%
 fprintf('\nRunning K-Means clustering on example dataset.\n\n');
 
 % Load an example dataset
@@ -81,10 +47,6 @@ load('ex7data2.mat');
 K = 3;
 max_iters = 10;
 
-% For consistency, here we set centroids to specific values
-% but in practice you want to generate them automatically, such as by
-% settings them to be random examples (as can be seen in
-% kMeansInitCentroids).
 initial_centroids = [3 3; 6 2; 8 5];
 
 % Run K-Means algorithm. The 'true' at the end tells our function to plot
@@ -94,14 +56,6 @@ fprintf('\nK-Means Done.\n\n');
 
 fprintf('Program paused. Press enter to continue.\n');
 % pause;
-
-%% ============= Part 4: K-Means Clustering on Pixels ===============
-%  In this exercise, you will use K-Means to compress an image. To do this,
-%  you will first run K-Means on the colors of the pixels in the image and
-%  then you will map each pixel on to it's closest centroid.
-%  
-%  You should now complete the code in kMeansInitCentroids.m
-%
 
 fprintf('\nRunning K-Means clustering on pixels from an image.\n\n');
 
@@ -137,22 +91,11 @@ initial_centroids = kMeansInitCentroids(X, K);
 fprintf('Program paused. Press enter to continue.\n');
 % pause;
 
-
-%% ================= Part 5: Image Compression ======================
-%  In this part of the exercise, you will use the clusters of K-Means to
-%  compress an image. To do this, we first find the closest clusters for
-%  each example. After that, we 
-
 fprintf('\nApplying K-Means to compress an image.\n\n');
 
 % Find closest cluster members
 idx = findClosestCentroids(X, centroids);
 
-% Essentially, now we have represented the image X as in terms of the
-% indices in idx. 
-
-% We can now recover the image from the indices (idx) by mapping each pixel
-% (specified by it's index in idx) to the centroid value
 X_recovered = centroids(idx,:);
 
 % Reshape the recovered image into proper dimensions
